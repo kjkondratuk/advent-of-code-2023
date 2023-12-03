9c1cd2f8d366d6a9ff6e1e8520b300162dc1c034
Kevin Kondratuk <kkondratuk@Kevins-MacBook-Pro.local>
2023-12-02 19:53:13 -0600

    start parsing

--- day-02/src/bin/day2_part1.rs
+++ day-02/src/bin/day2_part1.rs
@@ -2,10 +2,20 @@ extern crate helpers;
 
 use std::collections::HashMap;
 use helpers::lines;
+use crate::ParseError::{InvalidPrefixBlock};
 
-struct Game {
+struct Game<'a> {
     id: i32,
-    set: HashMap<str, i32>,
+    set: &'a HashMap<String, i32>,
+}
+
+impl<'a> Game<'a> {
+    fn new(id: i32) -> Game<'a> {
+        Self {
+            id,
+            set: &HashMap::new(),
+        }
+    }
 }
 
 fn main() {
@@ -21,13 +31,38 @@ fn main() {
 
 #[derive(Debug)]
 enum ParseError {
-
+    InvalidPrefixBlock(Vec<String>),
 }
 
 fn parse(lines: Vec<&str>) -> Result<Vec<Game>, ParseError> {
     let games = Vec::new();
     for l in lines {
+        let mut game;
+        let prefix_parts: Vec<String> = l
+            .split(":")
+            .map(|v| String::from(v))
+            .collect();
+        if prefix_parts.len() == 2 {
+            let game_prefix_parts: Vec<String> = prefix_parts.get(0).unwrap().split(" ")
+                .map(|v| String::from(v))
+                .collect();
+            if game_prefix_parts.len() == 2 {
+                let result = *game_prefix_parts.get(1).parse();
+                game = Game::new(result);
+                // TODO : fix types here
+            } else {
+                return Err(InvalidPrefixBlock(game_prefix_parts))
+            }
 
+            let draws_parts: Vec<String> = prefix_parts.get(1).unwrap().split(";").collect();
+            for draw in draws_parts {
+                let draws = draw.split(",").collect();
+                // TODO : left off here
+
+            }
+        } else {
+            return Err(InvalidPrefixBlock(prefix_parts))
+        }
     }
 
     Ok(games)
