2cf33da66ab64db1eefd72911f7faf21f938db59
Kevin Kondratuk <paintface07@gmail.com>
2023-12-03 00:40:03 -0600

    streamline approach a bit

--- day-02/src/bin/day2_part1.rs
+++ day-02/src/bin/day2_part1.rs
@@ -1,73 +1,96 @@
 extern crate helpers;
 
-use std::collections::HashMap;
+use crate::ParseError::{InvalidGameData, InvalidPrefixBlock};
 use helpers::lines;
-use crate::ParseError::{InvalidPrefixBlock};
 
-struct Game<'a> {
+struct Game {
     id: i32,
-    set: &'a HashMap<String, i32>,
+    set: Vec<Pull>,
 }
 
-impl<'a> Game<'a> {
-    fn new(id: i32) -> Game<'a> {
-        Self {
-            id,
-            set: &HashMap::new(),
-        }
+struct Pull {
+    red: i32,
+    green: i32,
+    blue: i32,
+}
+
+impl Game {
+    fn new(id: i32) -> Game {
+        Self { id, set: vec![] }
     }
 }
+
+// impl fmt::Display for Game {
+//     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+//         write!(f, "Game {{ id: {}, set: {{", self.id)?;
+//
+//         for (key, value) in &self.set {
+//             write!(f, " {}: {},", key, value)?;
+//         }
+//
+//         write!(f, " }} }}")
+//     }
+// }
 
 fn main() {
     let input = include_str!("input.txt");
     let lines = lines(input);
     let parse_result = parse(lines);
     if !parse_result.is_ok() {
-        eprintln!("an error occurred parsing the data: {:?}", parse_result.err())
+        eprintln!(
+            "an error occurred parsing the data: {:?}",
+            parse_result.as_ref().err()
+        )
     }
 
-    let result = process(parse_result.unwrap());
+    let _ = process(parse_result.unwrap());
 }
 
 #[derive(Debug)]
 enum ParseError {
-    InvalidPrefixBlock(Vec<String>),
+    InvalidPrefixBlock,
+    InvalidGameData,
 }
 
 fn parse(lines: Vec<&str>) -> Result<Vec<Game>, ParseError> {
     let games = Vec::new();
     for l in lines {
-        let mut game;
-        let prefix_parts: Vec<String> = l
-            .split(":")
-            .map(|v| String::from(v))
-            .collect();
-        if prefix_parts.len() == 2 {
-            let game_prefix_parts: Vec<String> = prefix_parts.get(0).unwrap().split(" ")
-                .map(|v| String::from(v))
-                .collect();
-            if game_prefix_parts.len() == 2 {
-                let result = *game_prefix_parts.get(1).parse();
-                game = Game::new(result);
-                // TODO : fix types here
-            } else {
-                return Err(InvalidPrefixBlock(game_prefix_parts))
-            }
+        let rec_parts: Vec<String> = l.split(":").map(|v| String::from(v)).collect();
 
-            let draws_parts: Vec<String> = prefix_parts.get(1).unwrap().split(";").collect();
-            for draw in draws_parts {
-                let draws = draw.split(",").collect();
-                // TODO : left off here
+        if rec_parts.len() == 2 {
+            let game_int = rec_parts
+                .get(0)
+                .unwrap()
+                .split(" ")
+                .map(|s| String::from(s))
+                .collect::<Vec<String>>()
+                .get(1)
+                .unwrap()
+                .parse::<i32>()
+                .unwrap();
+            println!("game_int: {}", game_int);
 
-            }
+            let sets = rec_parts
+                .get(1)
+                .unwrap()
+                .split("; ")
+                .map(|s| String::from(s))
+                .collect::<Vec<String>>()
+                .iter()
+                .map(|set| {
+                    let pull = set
+                        .split(", ")
+                        .map(|s| String::from(s))
+                        .collect()
+                        .
+
+                });
         } else {
-            return Err(InvalidPrefixBlock(prefix_parts))
+            return Err(InvalidPrefixBlock);
         }
     }
 
     Ok(games)
 }
 
-fn process(games: Vec<Game>) -> () {
-
-}
+fn process(games: Vec<Game>) -> () {}
