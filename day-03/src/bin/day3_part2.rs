8289d199ffb822fc4a4f5c661bbbdda5142a3d74
Kevin Kondratuk <k.kondratuk@fetchrewards.com>
2023-12-04 13:55:13 -0600

    get day 3 part 2 working

--- day-03/src/bin/day3_part2.rs
+++ day-03/src/bin/day3_part2.rs
@@ -28,12 +28,12 @@ fn parse(lines: Vec<&str>) -> Result<Vec<Block>, ParsingError> {
     let mut prev_line = "".to_string();
     // let mut next_line = "".to_string();
     for (i, l) in lines.iter().map(|&s| String::from(s)).enumerate() {
-        // TODO : working on fixing this
         let next = lines.get(i + 1);
-        let next_line = match next {
+        let mut next_line = match next {
             Some(n) => String::from(n.to_string()),
             None => "".to_string(),
         };
+        let mut curr_line = lines.get(i).unwrap();
         for cap in number_pattern.captures_iter(l.as_str()) {
             let value = cap.get(0).unwrap();
             let start = value.start();
@@ -54,7 +54,7 @@ fn parse(lines: Vec<&str>) -> Result<Vec<Block>, ParsingError> {
 
             // TODO : have to add some logic to size the bounding box so we don't truncate numbers surrounding the asterisk
             let curr_block =
-                String::from(&lines.get(i).unwrap().to_string().as_str()[block_range.clone()]);
+                String::from(&curr_line.to_string().as_str()[block_range.clone()]);
             let mut prev_block = "".to_string();
             if prev_line.len() > 0 {
                 prev_block = String::from(&prev_line.as_str()[block_range.clone()]);
@@ -64,15 +64,70 @@ fn parse(lines: Vec<&str>) -> Result<Vec<Block>, ParsingError> {
                 next_block = String::from(&next_line.as_str()[block_range.clone()]);
             }
 
+            let starts_with_number = Regex::new("^[0-9]+").unwrap();
+            let ends_with_number = Regex::new("[0-9]+$").unwrap();
+
+            let new_blocks: Vec<String> = vec![(prev_block.to_string(), &prev_line), (curr_block.to_string(), &curr_line.to_string()), (next_block.to_string(), &next_line)].iter().map(|(b, l)| {
+                let mut result = b.clone();
+                // if b == "6.2" {
+                //     println!("block: {}", b);
+                // }
+                // println!("block: {}", b);
+                if starts_with_number.is_match(b) {
+                    let mut start_char = b.get(0..1).unwrap().to_string();
+                    let mut offset = 0;
+                    let mut done = false;
+                    while !done && starts_with_number.is_match(start_char.as_str()) {
+                        // protection for decrementing a usize out of range
+                        if offset + 2 > start {
+                            done = true;
+                            break;
+                        }
+                        let range = start - 2 - offset..start - 1 - offset;
+                        let r = l.get(range);
+                        match r {
+                            Some(c) => {
+                                result = c.to_string() + result.as_str();
+                                start_char = c.to_string();
+                            },
+                            _ => done = true
+                        }
+                        offset = offset + 1;
+                    }
+                }
+
+                if ends_with_number.is_match(b) {
+                    let mut end_char = b.get(b.len() - 1..b.len()).unwrap().to_string();
+                    let mut offset = 0;
+                    let mut done = false;
+                    while !done && ends_with_number.is_match(end_char.as_str()) {
+                        let r = l.get(start + len + 1 + offset..start + len + 2 + offset);
+                        match r {
+                            Some(c) => {
+                                result = result.to_string() + c;
+                                end_char = c.to_string();
+                            },
+                            _ => done = true
+                        }
+                        offset = offset + 1;
+                    }
+                }
+
+                return result
+            }).collect::<Vec<String>>();
+
+            // println!("block: {}", new_blocks.concat());
+
             // let nbr = String::from(&lines.get(i).unwrap().to_string().as_str()[start..start + len])
             //     .parse::<i32>()
             //     .unwrap();
 
             let b = Block {
                 number: 0,
+                numbers: vec![],
                 start,
                 len,
-                data: vec![prev_block, curr_block, next_block],
+                data: new_blocks,
             };
 
             // println!("line: {}, number: {}, start: {}, len: {}, has_symbol: {}, data: {:?}", i, b.number, b.start, b.len, b.contains_symbol(), b.data);
@@ -86,19 +141,21 @@ fn parse(lines: Vec<&str>) -> Result<Vec<Block>, ParsingError> {
 }
 
 fn process(blocks: Vec<Block>) -> i32 {
-    let b = blocks.iter().filter(|&b| b.numbers().len() == 2).collect::<Vec<&Block>>();;
+    let b = blocks.iter().filter(|b| b.numbers().len() == 2).collect::<Vec<&Block>>();;
 
-    for x in &b {
-        println!("data: {}, numbers: {:?}, gear_ratio: {}", x.data.concat(), x.numbers(), x.numbers().get(0).unwrap() * x.numbers().get(1).unwrap());
-    }
+    // for x in &b {
+    //     println!("data: {}, numbers: {:?}, gear_ratio: {}", x.data.concat(), x.numbers(), x.numbers().get(0).unwrap() * x.numbers().get(1).unwrap());
+    // }
 
     b.iter().map(|b| b.numbers().get(0).unwrap() * b.numbers().get(1).unwrap()).sum()
+    // 0
 }
 
 struct Block {
     number: i32,
     start: usize,
     len: usize,
+    numbers: Vec<i32>,
     data: Vec<String>,
 }
 
@@ -111,7 +168,9 @@ impl Block {
             }
         }
 
-        return nbr_strs
+        // let n = self.numbers.clone();
+        // return n
+        nbr_strs
     }
 }
 
@@ -122,9 +181,9 @@ impl fmt::Display for Block {
             "Block: number: {}, start: {}, len: {}, chars: ",
             self.number, self.start, self.len
         )?;
-        for d in &self.data {
-            write!(f, "{}", d)?;
-        }
+        // for d in &self.data {
+        //     write!(f, "{}", d)?;
+        // }
         write!(f, "")
     }
 }
