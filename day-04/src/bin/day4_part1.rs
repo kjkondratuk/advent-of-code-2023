ec7e451f9ceae4d3b454959618c0fc5cec731a42
Kevin Kondratuk <paintface07@gmail.com>
2023-12-09 20:07:19 -0600

    walk back day 4 part 2 nonsense that leaked into part 1

--- day-04/src/bin/day4_part1.rs
+++ day-04/src/bin/day4_part1.rs
@@ -4,7 +4,7 @@ use helpers::lines;
 use std::string::ParseError;
 
 fn main() {
-    let input = include_str!("part_1.txt");
+    let input = include_str!("input.txt");
     let lines = lines(input);
     let parse_result = parse(lines);
     let v = match parse_result {
@@ -22,7 +22,6 @@ struct Card {
     id: i32,
     winning_numbers: Vec<i32>,
     card_numbers: Vec<i32>,
-    copies: i32,
 }
 
 impl fmt::Display for Card {
@@ -32,9 +31,6 @@ impl fmt::Display for Card {
             "Card: id: {}, winning_numbers: {:?}, card_numbers: {:?}",
             self.id, self.winning_numbers, self.card_numbers
         )?;
-        // for d in &self.data {
-        //     write!(f, "{}", d)?;
-        // }
         write!(f, "")
     }
 }
@@ -89,88 +85,35 @@ fn parse(lines: Vec<&str>) -> Result<Vec<Card>, ParseError> {
             id: game_id,
             winning_numbers,
             card_numbers,
-            copies: 1,
         })
     }
 
     Ok(games)
 }
 
-fn process(mut data: Vec<Card>) -> i32 {
-    let mut idx = 0;
-    // let mut card_count = 0;
+fn process(data: Vec<Card>) -> i32 {
+    let mut acc = 0;
 
     // Make a COPY of ALL OUR FUCKING DATA because the rust compiler is a grumpy old man.
     for card in data.clone().into_iter() {
         // println!("incrementing card counter: {}", card_count);
-        // card_count += 1;
-        // let mut score = 0;
+        let mut score = 0;
         let winner_ct = card.card_numbers
             .iter()
             .filter(|c| card.winning_numbers.iter().find(|c1| c == c1).is_some())
             .count();
-        // println!("{}", card);
-        println!("card: {}, winner_ct: {}", card.id, winner_ct);
 
-        // card_count += winner_ct;
-
-        // Increment copies
-        let mut done = false;
-        while !done {
-            increment_impacted(&mut data, idx, 0, &mut done);
+        if winner_ct == 1 {
+            // println!("adding points: {}", 1);
+            score = score + 1;
         }
-        // for upcoming in idx+1..idx+1+winner_ct {
-        //     let mut up_card = data[upcoming].clone();
-        //     up_card.copies = up_card.copies + data[upcoming].copies;
-        //     // println!("applying card: {}, mulitplier: {}", up_card.id, up_card.copies);
-        //     // card_count += up_card.copies;
-        //     data[upcoming] = up_card;
-        //
-        //     // println!("game: {}, applying copies: {}", data[upcoming].id, data[upcoming].copies);
-        // }
+        if winner_ct > 1 {
+            // println!("adding points: {}", 2_i32.pow((winner_ct - 1) as u32));
+            score = 2_i32.pow((winner_ct - 1) as u32);
+        }
 
-        // if winner_ct >= 1 {
-        //     println!("adding points: {}", 1);
-        //     score = score + 1;
-        // }
-        // if winner_ct > 1 {
-        //     println!("adding points: {}", 2_i32.pow((winner_ct - 1) as u32));
-        //     score = 2_i32.pow((winner_ct - 1) as u32);
-        // }
-
-        // have to reference the fucking mutable one instead of the copy we're looping over...  Rust is dumb sometimes...
-        // acc = acc + score * data[idx].copies;
-        idx += 1;
+        acc = acc + score;
     }
 
-    let mut acc:i32 = 0;
-    for card in data {
-        println!("card: {}, copies: {}", card.id, card.copies);
-        acc = acc + 1 + card.copies;
-    }
     acc
-    // card_count
-    // 0
-}
-
-fn increment_impacted(mut data: &mut Vec<Card>, start_idx: usize, mut index: usize, mut done: bool) -> () {
-    let c = data.get_mut(start_idx + index);
-    match c {
-        Some(card) => {
-            let winner_ct = card.card_numbers
-                .iter()
-                .filter(|c| card.winning_numbers.iter().find(|c1| c == c1).is_some())
-                .count();
-            card.copies += 1;
-            //     up_card.copies = up_card.copies + data[upcoming].copies;
-            //     // println!("applying card: {}, mulitplier: {}", up_card.id, up_card.copies);
-            //     // card_count += up_card.copies;
-            //     data[upcoming] = up_card;
-            index += 1;
-        }
-        None => {
-            done = true;
-        }
-    }
-
 }
